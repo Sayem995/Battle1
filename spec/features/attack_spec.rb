ac97a03f056800d76d3bdb7733136c7cba7b1b41
Sayem995 <sayem.bokhari.1@gmail.com>
2020-01-01 14:00:23 +0000

    User story 5 failing test

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,17 @@
 feature 'Attacking' do
+
   scenario 'attack Player 2' do
     sign_in_and_play
     click_button "Attack"
     expect(page).to have_content 'Sayem attacked Hisham'
   end
+
+  scenario 'reduce Player 2s HPs by 10' do
+    sign_in_and_play
+    click_button "Attack"
+    click_link "Ok"
+    expect(page).not_to have_content 'Hisham: 60HP'
+    expect(page).to have_content 'Hisham: 50HP'
+  end
+
 end
