b9714dd5643cc3a821a9f825dc1cc858c7a3c54f
Sayem995 <sayem.bokhari.1@gmail.com>
2020-01-01 14:25:34 +0000

    All tests passing for user story 5 except 1

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,15 +1,9 @@
 feature 'Attacking' do
 
-  scenario 'attack Player 2' do
+  scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play
     click_button "Attack"
-    expect(page).to have_content 'Sayem attacked Hisham'
-  end
-
-  scenario 'reduce Player 2s HPs by 10' do
-    sign_in_and_play
-    click_button "Attack"
-    click_link "Ok"
+    click_link 'OK'
     expect(page).not_to have_content 'Hisham: 60HP'
     expect(page).to have_content 'Hisham: 50HP'
   end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,31 @@
 require 'player'
 
 describe Player do
-  subject(:Sayem) { Player.new('Sayem') }
+  subject(:sayem) { Player.new('Sayem') }
+  subject(:hisham) { Player.new('Hisham') }
 
   describe '#name' do
     it 'returns the name' do
-      expect(subject.name).to eq 'Sayem'
+      expect(sayem.name).to eq 'Sayem'
     end
   end
-end
+
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(sayem.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(hisham).to receive(:receive_damage)
+      sayem.attack(hisham)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { sayem.receive_damage }.to change { sayem.hit_points }.by(-10)
+    end
+  end
+ end
